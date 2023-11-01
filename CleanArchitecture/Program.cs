e5c1354dd7150d1270303590aa498194fa84bef0
Juan Carmona <juan_carmona@epam.com>
2023-11-01 11:05:40 -0500

    adding cors policy to validate conections from web apps

--- CleanArchitecture/Program.cs
+++ CleanArchitecture/Program.cs
@@ -10,7 +10,19 @@ builder.Services.AddPresentation();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
+
+//Adding a cors policy to allow connections from web apps
+builder.Services.AddCors(options =>
+{
+    /*
+    options.AddPolicy("AllowMyApp",
+        policy => policy
+            .WithOrigins("https://example.com"))
+    */
+    options.AddPolicy("AllowMyApp",
+        policy => policy
+            .AllowAnyOrigin());
+});
 
 var app = builder.Build();
 
@@ -23,9 +35,15 @@ if (app.Environment.IsDevelopment())
 else
 {
     //HTTP Strict Transport Security(HSTS)
+    //Is a header returned by the server (this API in this case) and instructs web browsers to prevent users from connecting over HTTP
+    //However trying from the browser it redirect automatically from http port to the https port
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Returns a 300 status code with a redirect response pointing them at the https port
+app.UseHttpsRedirection();
+app.UseCors("AllowMyApp");
 
 app.MapControllers();
 
