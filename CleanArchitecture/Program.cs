4184f7ffddb1001ac4e0b817a38111c382f73a82
Juan Carmona <juan_carmona@epam.com>
2023-11-01 10:27:16 -0500

    adding HSTS to prevent connections using http from browsers in production

--- CleanArchitecture/Program.cs
+++ CleanArchitecture/Program.cs
@@ -20,6 +20,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    //HTTP Strict Transport Security(HSTS)
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.MapControllers();
 
